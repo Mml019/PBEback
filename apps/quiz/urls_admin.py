1377469d0a2073504f92114391a79a6318dafbd1
Marta <missmartamartin@gmail.com>
2026-02-05 01:22:48 +0100

    base.html, urls afmin y view admin

--- apps/quiz/urls_admin.py
+++ apps/quiz/urls_admin.py
@@ -4,7 +4,7 @@ from .views_admin import *
 # Django Endpoint is quiz-admin/
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login-admin'),
-    path('logout/', LogoutView.as_view(), name='logoutn-admin'),
+    path('logout/', LogoutView.as_view(), name='logout-admin'),
     # path('register/', Registration.as_view(), name='register-admin'),
     path('quizes/', QuizViews.as_view(), name='show-quizes')
 ]
